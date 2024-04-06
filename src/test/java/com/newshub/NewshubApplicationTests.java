8a3f60d02137be05cc2be2f4823e1d76cbf3e7b0
Eugene Jang <kyunellroll@gmail.com>
2024-04-06 16:29:17 +0900

    feat(...): init setting

---
+++ src/test/java/com/newshub/NewshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
